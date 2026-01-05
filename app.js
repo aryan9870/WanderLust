72082b962fe78b289654223ecb3628319333fda8
Aryan <aryan7017n@gmail.com>
2026-01-05 13:13:56 +0530

    Fixed edit form

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-if(process.env.NODE_ENV != "production") {
-  require('dotenv').config()
-}
-
-console.log(process.env.CLOUD_NAME);
-
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
